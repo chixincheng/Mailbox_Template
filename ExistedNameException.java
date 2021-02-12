ae1cb1dd239267aafd9e221fbfde4e7ed490b561
chixincheng <chixincheng@gmail.com>
2021-02-12 14:14:04 -0500

    Update ExistedNameException.java

--- ExistedNameException.java
+++ ExistedNameException.java
@@ -3,9 +3,7 @@
  * the name entered/given already exist.
  * 
  * @author
- *      XinCheng Chi, SBU ID#：111919385,Recition R30
- * Assignment:
- *      Homework #5 for CSE 214, Summer 2020
+ *      XinCheng Chi
  * Date:
  *      August 6,2020
  */
