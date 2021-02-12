f7f5beb849d72dd1d4c965d678c9a3aa2a18d681
chixincheng <chixincheng@gmail.com>
2021-02-12 14:14:24 -0500

    Update Mailbox.java

--- Mailbox.java
+++ Mailbox.java
@@ -14,9 +14,7 @@ import java.util.GregorianCalendar;
  * manipulate the folders
  * 
  * @author
- *      XinCheng Chi, SBU ID#：111919385,Recition R30
- * Assignment:
- *      Homework #5 for CSE 214, Summer 2020
+ *      XinCheng Chi
  * Date:
  *      August 6,2020
  */
